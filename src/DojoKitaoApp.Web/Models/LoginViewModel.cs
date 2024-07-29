bdaf768f46675b4a5f65fdcb53bc3a0f816523ff
Roberwal Junior <roberwal.junior@outlook.com>
2024-07-29 10:04:06 -0300

    Corrigindo erro ao fazer login.

--- src/DojoKitaoApp.Web/Models/LoginViewModel.cs
+++ src/DojoKitaoApp.Web/Models/LoginViewModel.cs
@@ -6,8 +6,10 @@ public class LoginViewModel
 {
     [Required(ErrorMessage = "{0} é obrigatório!")]
     [Display(Name = "E-mail")]
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
 
     [Required(ErrorMessage = "{0} é obrigatório!")]
+    [DataType(DataType.Password)]
     public string? Senha { get; set; }
 }
